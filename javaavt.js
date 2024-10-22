0c39ae884f52122fd87b13671ddb13d70cf1d523
Ethan Gomes <gomes.ethan02@gmail.com>
2024-10-22 19:46:55 +0200

    JavaAdd

--- javaavt.js
+++ javaavt.js
@@ -1,21 +1,22 @@
-/*
-function changeImage(imageSrc) {
-    const mainImage = document.getElementById('main-image');
-    mainImage.src = imageSrc;
-}
-    */
 
-// Function to change the main image
+
+var scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 800,  
+    speedAsDuration: true  
+});
+
 function changeImage(imageId, imagePath) {
     document.getElementById(imageId).src = imagePath;
 }
 
-// Function to toggle zoom on the clicked image
+
 function toggleZoom(imageId) {
     const imageElement = document.getElementById(imageId);
     if (imageElement.classList.contains('zoomed')) {
-        imageElement.classList.remove('zoomed'); // Remove zoom
+        imageElement.classList.remove('zoomed'); 
     } else {
-        imageElement.classList.add('zoomed'); // Apply zoom
+        imageElement.classList.add('zoomed'); 
     }
 }
+
+
